bbad1164a4c969157b8ea0fee6edfbea8653283e
chrislucas <christoffer.luccas@gmail.com>
2020-03-06 08:07:57 -0300

    testando o metodo que recupera a lista de pontos batidos por um usuario usando o seu ID

--- sample-restapi/demo/src/test/java/br/com/sample/restapi/controller/CheckPointControllerTest.java
+++ sample-restapi/demo/src/test/java/br/com/sample/restapi/controller/CheckPointControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -52,6 +53,35 @@ public class CheckPointControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$", Matchers.hasSize(Matchers.equalTo(list.size()))))
 			.andExpect(jsonPath("$[0].date", is(checkPoint.getDate())));
+			;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void givenAUserIdGetAListOfUsersCheckpoints() {
+		List<CheckPoint> list = Arrays.asList(
+				 new CheckPoint(1, 1, 10000000000000L, true)
+				,new CheckPoint(2, 1, 10000000000001L, false)
+				,new CheckPoint(3, 2, 10000000000000L, true)
+				,new CheckPoint(4, 2, 10000000000001L, false)
+			);
+		
+		int userId = 1;
+		List<CheckPoint> filtered = list.stream()
+				.filter( it -> it.getUserId() == userId)
+				.collect(Collectors.toList());
+		when(service.getByUserId(userId)).thenReturn(filtered);
+		
+		try {
+			CheckPoint checkPoint = filtered.get(0);
+			mvc.perform(get("/app/checkpoints")
+					.contentType(MediaType.APPLICATION_JSON)
+				)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", Matchers.hasSize(Matchers.equalTo(filtered.size()))))
+			.andExpect(jsonPath("$[0].userId", is(checkPoint.getUserId())));
 			;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
